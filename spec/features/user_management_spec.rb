16179b1ad5b52e5c5ef8b5e404e6f612e168198b
shelleyhope <shelleyandkenny@mac.com>
2014-05-07 20:53:55 +0100

    need to sort out where the Good Bye notice goes to complete sign out feature

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
+require_relative 'helpers/session'
+
+include SessionHelpers
 
 
 feature "User signs up" do
@@ -22,16 +25,6 @@ feature "User signs up" do
 
   scenario "with a password that doesn't match" do
     lambda { sign_up('a@a.com', 'pass', 'wrong') }.should change(User, :count).by(0)    
-  end
-
-  def sign_up(email = "alice@example.com", 
-              password = "oranges!", 
-              password_confirmation = "oranges!")
-    visit '/users/new'
-    fill_in :email, :with => email
-    fill_in :password, :with => password
-    fill_in :password_confirmation, :with => password_confirmation
-    click_button "Sign up"
   end
 
   scenario "with an email that is already registered" do    
@@ -63,12 +56,21 @@ feature "User signs in" do
     sign_in('test@test.com', 'wrong')
     expect(page).not_to have_content("Welcome, test@test.com")
   end
+end
 
-  def sign_in(email, password)
-    visit '/sessions/new'
-    fill_in 'email', :with => email
-    fill_in 'password', :with => password
-    click_button 'Sign in'
+feature 'User signs out' do
+
+  before(:each) do
+    User.create(:email => "test@test.com", 
+                :password => 'test', 
+                :password_confirmation => 'test')
+  end
+
+  scenario 'while being signed in' do
+    sign_in('test@test.com', 'test')
+    click_button "Sign out"
+    expect(page).to have_content("Good bye!") # where does this message go?
+    expect(page).not_to have_content("Welcome, test@test.com")
   end
 
 end
